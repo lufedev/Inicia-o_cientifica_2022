2a735abd6c7b41ef7c59f62b9e0ab8018db2c68e
lufedev <felipeluizsantos.k@gmail.com>
2023-04-18 19:29:05 -0300

    Correlation Update

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from ydata_profiling import ProfileReport
 
 conn = psycopg2.connect(
-    "dbname = 'postgres' user = 'postgres' host = '192.168.15.45' port = '7777' password = 'ic_2023'"
+    "dbname = 'postgres' user = 'postgres' host = 'localhost' port = '7777' password = 'ic_2023'"
 )
 
 
@@ -76,15 +76,16 @@ df_analfabetismo_2019 = getAnalfabetismo("2019")
 df_analfabetismo_2020 = getAnalfabetismo("2020")
 df_analfabetismo_2021 = getAnalfabetismo("2021")
 
-df_2019_comparative = pd.merge(df_2019, df_analfabetismo_2019, on="UF")
-df_2020_comparative = pd.merge(df_2020, df_analfabetismo_2020, on="UF")
-df_2021_comparative = pd.merge(df_2021, df_analfabetismo_2021, on="UF")
 
-# UF_grouped = df_2019_comparative.groupby("UF")
-# for uf, grupo in UF_grouped:
-#     tabela_uf = grupo.copy()
-#     tabela_uf["UF"] = tabela_uf["UF"].apply(lambda x: f"{x} 2019")
-#     tabela_uf.to_csv(f"./dados/Filtered/2019/{uf}-2019.csv", index=False)
+df_2019_comparative = pd.merge(df_2019, df_analfabetismo_2019.rename(columns={"2019":"Analfabetismo"}), on="UF")
+df_2020_comparative = pd.merge(df_2020, df_analfabetismo_2020.rename(columns={"2020":"Analfabetismo"}), on="UF")
+df_2021_comparative = pd.merge(df_2021, df_analfabetismo_2021.rename(columns={"2021":"Analfabetismo"}), on="UF")
+
+UF_grouped = df_2020_comparative.groupby("UF")
+for uf, grupo in UF_grouped:
+    tabela_uf = grupo.copy()
+    tabela_uf["UF"] = tabela_uf["UF"].apply(lambda x: f"{x} 2020")
+    tabela_uf.to_csv(f"./dados/Filtered/2020/{uf}-2020.csv", index=False)
 
 
 Acre2019 = pd.read_csv("./dados/Filtered/2019/Acre-2019.csv")
@@ -92,5 +93,17 @@ Acre2020 = pd.read_csv("./dados/Filtered/2020/Acre-2020.csv")
 Acre2021 = pd.read_csv("./dados/Filtered/2021/Acre-2021.csv")
 
 Acre = Acre2019.append((Acre2020, Acre2021), ignore_index=True)
-profile = ProfileReport(Acre, title="report_acre")
-profile.to_file("report.html")
+Acre = Acre.fillna(0)
+
+Acre = Acre.drop(columns=['UF'])
+print(Acre['Analfabetismo'])
+
+print(Acre.corr(Acre['Analfabetismo']))
+# Acre_report = ProfileReport(Acre, title="Report")
+# Acre_report.to_file("report.html")
+# #Make the plot
+# plt.bar(Acre.iloc[0:1])
+# plt.bar(Acre.iloc[1:2])
+# plt.bar(Acre.iloc[2:3])
+# plt.xlabel('Ano')
+# plt.show('')

d165d7a9ca8955a5af43c964bb510e068e130a7d
Luiz <felipeluizsantos.k@gmail.com>
2023-05-10 08:47:25 -0300

    tabela nova

--- analysis.py
+++ analysis.py
@@ -7,6 +7,17 @@ import functools as ft
 import os
 from ydata_profiling import ProfileReport
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split  # Import train_test_split function
+from sklearn.tree import export_graphviz
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+from sklearn import (
+    metrics,
+)  # Import scikit-learn metrics module for accuracy calculation
+
+# Load libraries
 
 # COISAS IMPORTANTES
 diretorio = "./dados/Filtered/"
@@ -23,84 +34,139 @@ dfs_comparativos = {}
 UF = {}
 
 
-# QUERIES
-def getUF(year):
-    query = 'select * from "uf_' + year + '";'
+# # QUERIES
+def getByYear(year):
+    query = 'select * from "raw_data" WHERE ano=' + year + ";"
     return sqlio.read_sql_query(query, conn)
 
 
-def getAnalfabetismo(year):
-    query = 'select "UF","' + year + '" from "analfabetismo";'
-    return sqlio.read_sql_query(query, conn)
+# def getAnalfabetismo(year):
+#     query = 'select "UF","' + year + '" from "analfabetismo";'
+#     return sqlio.read_sql_query(query, conn)
 
 
-def getRenda(year):
-    query = 'select "UF","' + year + '" from "rendapercapita";'
-    return sqlio.read_sql_query(query, conn)
+# def getRenda(year):
+#     query = 'select "UF","' + year + '" from "rendapercapita";'
+#     return sqlio.read_sql_query(query, conn)
 
 
-def genFile(ano, dfs_comparativos):
-    for ano, df_comparativo in dfs_comparativos.items():
-        UF_grouped = df_comparativo.groupby("UF")
-    for uf, grupo in UF_grouped:
-        tabela_uf = grupo.copy()
-        tabela_uf["UF"] = tabela_uf["UF"].apply(lambda x: f"{x} {ano}")
-        tabela_uf.to_csv(f"./dados/Filtered/{ano}/{uf}-{ano}.csv", index=False)
+# def genFile(ano, dfs_comparativos):
+#     for ano, df_comparativo in dfs_comparativos.items():
+#         UF_grouped = df_comparativo.groupby("UF")
+#     for uf, grupo in UF_grouped:
+#         tabela_uf = grupo.copy()
+#         tabela_uf["UF"] = tabela_uf["UF"].apply(lambda x: f"{x} {ano}")
+#         tabela_uf.to_csv(f"./dados/Filtered/{ano}/{uf}-{ano}.csv", index=False)
 
 
-# Coleta a cv anual
-for ano in anos:
-    df_ufs[ano] = getUF(ano)
-    df_analfabetismo[ano] = getAnalfabetismo(ano)
-    df_renda[ano] = getRenda(ano)
+dfAno = getByYear("2010")
+colunas = []
+for coluna in dfAno.columns:
+    if (
+        coluna == "regiao_geo"
+        or coluna == "nomemun"
+        or coluna == "capital"
+        or coluna == "id_munic_7"
+        or coluna == "id_estado"
+        or coluna == "estado_abrev"
+        or coluna == "estado"
+        or coluna == "regiao"
+        or coluna == "no_regiao"
+        or coluna == "macrorregiao"
+        or coluna == "no_macro"
+    ):
+        continue  # pular a coluna3
+    colunas.append(coluna)
 
-# Mescla a tabela de analfabetismo com a tabela de CV, renomeia a coluna que contém o ano para Analfabetismo
-for ano in anos:
-    dfs = [
-        df_ufs[ano],
-        df_analfabetismo[ano].rename(columns={ano: "Analfabetismo"}),
-        df_renda[ano].rename(columns={ano: "Renda_per_capita"}),
-    ]
+X = dfAno[colunas]
+y = dfAno["id_estado"]
 
-    dfs_comparativos[ano] = ft.reduce(
-        lambda left, right: pd.merge(left, right, on="UF"), dfs
-    )
-# Exporta individualmente cada estado brasileiro, separando por ano.
-# genFile(ano, dfs_comparativos)
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1
+)  # 70% training and 30% test
+
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier()
+
+# Train Decision Tree Classifer
+clf = clf.fit(X_train, y_train)
+
+# Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+# dot_data = StringIO()
+# export_graphviz(
+#     clf,
+#     out_file=dot_data,
+#     filled=True,
+#     rounded=True,
+#     special_characters=True,
+#     feature_names=colunas,
+#     class_names=["0", "1"],
+# )
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+# graph.write_png("cv_vacinal.png")
+# Image(graph.create_png())
 
 
-# Agrupa todos os resultados por Estado.
-for subdir, _, files in os.walk(diretorio):
-    for file in files:
-        if file.endswith(".csv"):
-            caminho_arquivo = os.path.join(subdir, file)
-            # obter o nome do estado a partir do nome do arquivo
-            estado = file.split("-")[0]
-            # ler a tabela
-            tabela = pd.read_csv(caminho_arquivo)
-            # adicionar a coluna "UF" com o nome do estado
-            tabela["UF"] = estado
-            # adicionar a tabela no dicionário de UF
-            if estado in UF:
-                UF[estado] = pd.concat([UF[estado], tabela])
-            else:
-                UF[estado] = tabela
-# Remove a coluna UF
-for estado, tabela in UF.items():
-    tabela = tabela.fillna(0)
-    tabela = tabela.drop(columns=["UF"])
-    UF[estado] = tabela
+def old_stuff():
+    print("")
+    # # Coleta a cv anual
+    # for ano in anos:
+    #     df_ufs[ano] = getUF(ano)
+    #     df_analfabetismo[ano] = getAnalfabetismo(ano)
+    #     df_renda[ano] = getRenda(ano)
 
-x = np.array(UF["Sao Paulo"]["Analfabetismo"]).reshape((-1, 1))
-y = np.array(UF["Sao Paulo"]["BCG"])
+    # # Mescla a tabela de analfabetismo com a tabela de CV, renomeia a coluna que contém o ano para Analfabetismo
+    # for ano in anos:
+    #     dfs = [
+    #         df_ufs[ano],
+    #         df_analfabetismo[ano].rename(columns={ano: "Analfabetismo"}),
+    #         df_renda[ano].rename(columns={ano: "Renda_per_capita"}),
+    #     ]
 
-# https://realpython.com/linear-regression-in-python/
+    #     dfs_comparativos[ano] = ft.reduce(
+    #         lambda left, right: pd.merge(left, right, on="UF"), dfs
+    #     )
+    # # Exporta individualmente cada estado brasileiro, separando por ano.
+    # # genFile(ano, dfs_comparativos)
 
-model = LinearRegression().fit(x, y)
-r_sq = model.score(x, y)
-print(f"R²: {r_sq}")
+    # # Agrupa todos os resultados por Estado.
+    # for subdir, _, files in os.walk(diretorio):
+    #     for file in files:
+    #         if file.endswith(".csv"):
+    #             caminho_arquivo = os.path.join(subdir, file)
+    #             # obter o nome do estado a partir do nome do arquivo
+    #             estado = file.split("-")[0]
+    #             # ler a tabela
+    #             tabela = pd.read_csv(caminho_arquivo)
+    #             # adicionar a coluna "UF" com o nome do estado
+    #             tabela["UF"] = estado
+    #             # adicionar a tabela no dicionário de UF
+    #             if estado in UF:
+    #                 UF[estado] = pd.concat([UF[estado], tabela])
+    #             else:
+    #                 UF[estado] = tabela
+    # # Remove a coluna UF
+    # for estado, tabela in UF.items():
+    #     tabela = tabela.fillna(0)
+    #     tabela = tabela.drop(columns=["UF"])
+    #     UF[estado] = tabela
 
-y_pred = model.predict(x)
-print(x, y)
-print(UF["Acre"])
-print(f"predicted response:\n{y_pred}")
+    # x = np.array(UF["Sao Paulo"]["Analfabetismo"]).reshape((-1, 1))
+    # y = np.array(UF["Sao Paulo"]["BCG"])
+
+    # # https://realpython.com/linear-regression-in-python/
+
+    # model = LinearRegression().fit(x, y)
+    # r_sq = model.score(x, y)
+    # print(f"R²: {r_sq}")
+
+    # y_pred = model.predict(x)
+    # print(x, y)
+    # print(UF["Acre"])
+    # print(f"predicted response:\n{y_pred}")
--- dados/remover_acento.py
+++ dados/remover_acento.py
@@ -1,8 +1,9 @@
 import unidecode
 import csv
 import sys
+
 feed = sys.argv[1]
-csv_f = open(feed, encoding="latin-1", mode="r")
+csv_f = open(feed, mode="r")
 csv_str = csv_f.read()
 csv_str_removed_accent = unidecode.unidecode(csv_str)
 csv_f.close()
